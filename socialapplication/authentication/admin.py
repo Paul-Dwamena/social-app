fd37a54e5f7679b7f297fd8b213b4cae80603c92
Paul-Dwamena <pauldwamena10@gmail.com>
2023-08-27 22:37:33 +0000

    new-commit

--- socialapplication/authentication/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-@admin.register(User)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('id','username', 'email', 'following_count', 'follower_count')
